63843fb1a8cca3a55638d4153060a8f4dc6df8b2
LONG357 <LONG4357@gmail.com>
2023-03-12 11:22:19 +0700

    push phim 1

---
+++ Phim1/Phim1/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Phim1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Trangchu()
+        {
+            return View();
+        }
+
+        public ActionResult Thanhvien()
+        {
+            return View();
+        }
+        public ActionResult Muave()
+        {
+            return View();
+        }
+        public ActionResult Hotro()
+        {
+            return View();
+        }
+        public ActionResult Noidung()
+        {
+            return View();
+        }
+      
+        public ActionResult Dangnhap()
+        {
+            return View();
+        }
+        public ActionResult Dangky()
+        {
+            return View();
+        }
+    }
+}
---
+++ Phim1/Phim1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Phim1.MvcApplication" Language="C#" %>
